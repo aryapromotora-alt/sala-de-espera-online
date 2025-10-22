c6f586679678cb987896d5533b5f3a45c7ec2e42
aryapromotora-alt <eduardomurtada@gmail.com>
2025-10-22 18:08:01 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    autoprefixer()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
